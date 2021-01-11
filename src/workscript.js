6d6796ef046895061f52db6fd3fb3818c94a48b1
taras-vlas <68924583+taras-vlas@users.noreply.github.com>
2021-01-11 06:48:25 +0300

    +work

--- src/workscript.js
+++ src/workscript.js
@@ -1,11 +1,11 @@
 
-import galleryItems from "../src/gallery-items.js"; /* імпорт функції default в ІНШИЙ файл script
-                                                 "d:/GitHub/goit-js-hw-08-gallery/src/gallery-items" */
+import galleryItems from "./gallery-items.js"; 
 
 const galleryRef = document.querySelector(".gallery");  /*шукаємо список .gallery зі значення елем. по id */
-const lightBoxRef = document.querySelector(".lightbox");
-const substImgRef = document.querySelector(".lightbox__image");                    /*замінник */
+const lightBoxRef = document.querySelector(".lightbox");         /*modal */
+const substImgRef = document.querySelector(".lightbox__image");  /*замінник */
 const closeBtnRef = document.querySelector("button[data-action='close-lightbox']");
+  const overlayRef = document.querySelector(".lightbox__overlay");
 
 /*Ссилка на оригінал зображ. - data-атрибут source. А в href ссилки (це для доступності) */
 const imagesElement = galleryItems.reduce((acc, img) => {    
@@ -22,12 +22,13 @@ const imagesElement = galleryItems.reduce((acc, img) => {
                     />
             </a>
         </li>`);
+    console.log("acc", acc);
     return acc += img;
 }, '');
+     
 
 
 galleryRef.insertAdjacentHTML('afterbegin', imagesElement);
-
 
 const openLightBox = (event) => {
     if (event.target === event.currentTarget) {
@@ -37,15 +38,46 @@ const openLightBox = (event) => {
     lightBoxRef.classList.add('is-open');
     /*  ('src')  */
     substImgRef.setAttribute('src', `${event.target.getAttribute('data-source')}`);
+    substImgRef.setAttribute('alt', `${event.target.getAttribute('alt')}`);
 }
 
 const closeLightBox = (event) => {
     if (lightBoxRef.classList.contains('is-open')) {
+ 
         lightBoxRef.classList.remove('is-open');
-        }
+     // При закрытии попапа нужно очищать значение атрибутов src, alt
+        substImgRef.removeAttribute('src', `${event.target.removeAttribute('data-source')}`);
+        substImgRef.removeAttribute('alt', `${event.target.removeAttribute('alt')}`);
+    }
     return;
 }
 
 galleryRef.addEventListener('click', openLightBox);
 closeBtnRef.addEventListener('click', closeLightBox);
+
+  overlayRef.addEventListener('click', closeLightBox);
+
+
+  const closeByKey = (event) => {
+      if (event.keyCode === 27);       /* код кл. 'Escape'  https://keycode.info//*/ 
+    lightBoxRef.classList.remove('is-open');
+  }
+
+  document.addEventListener('keydown', closeByKey);
+
+
+
+  const rightArrowKey = (event) => {
+    if (event.keyCode === 39)        /* код кл. 'rightArrow'  https://keycode.info//*/ 
+      substImgRef.removeAttribute('src', `${event.target.removeAttribute('data-source')}`,'alt', `${event.target.removeAttribute('alt')}`);
+    
+  }
+  
+
+
+  const leftArrowKey = (event) => {
+    if (event.keyCode === 37)        /* код кл. 'leftArrow'  https://keycode.info//*/ 
+  
+     substImgRef.removeAttribute('src', `${event.target.removeAttribute('data-source')}`); 
+  }
 
